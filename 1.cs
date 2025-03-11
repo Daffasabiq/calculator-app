daf4259a67b277414b5a5a6a8aeed8d63f7180e4
Daffasabiq <daffasabiq@amikom.students.ac.id>
2025-03-11 14:10:36 +0700

    Penambahan code pengurangan

--- 1.cs
+++ 1.cs
@@ -2,11 +2,24 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-	
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("HasilPenambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("HasilPengurangan: {0} - {1} = {2}", a, b, Penambahan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
+		
 	}
 	
 	static int Penambahan (int a, int b)
 	{
 		return a + b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
